17c4d05ef9e87f1bdc6d1af45e5b3be154c81a52
GuillermoHra <guillermo_h95@hotmail.com>
2018-04-27 19:47:28 -0500

    Everything done

--- StateMachine.m
+++ StateMachine.m
@@ -11,6 +11,10 @@ clear
 clc
 
 a = arduino('COM20', 'Mega2560');
+aPositionPin = 'A7';
+aForcePin = 'A8';
+dForwardPin = 'D10';
+dBackwardPin = 'D11';
 pos = [];
 pos_cont = 1;
 force = [];
@@ -32,7 +36,7 @@ while(1)
         T = .0338;
 
         % Initialization of first samples
-        v = readVoltage(a,'A7');
+        v = readVoltage(a,aPositionPin);
         c(1) = v;
         pos(pos_cont) = c(1);
         pos_cont = pos_cont + 1;
@@ -40,37 +44,37 @@ while(1)
         e(1) = r(1)-c(1);
         m(1) = (1/(2*T))*((2*T*Kp+Ki*(T^2)+4*Kd)*e(1));
         if abs(m(1)) > 5
-            writePWMVoltage(a, 'D10', 5);
-            writePWMVoltage(a, 'D11', 0);
+            writePWMVoltage(a, dForwardPin, 5);
+            writePWMVoltage(a, dBackwardPin, 0);
         elseif abs(m(1)) > 2
-            writePWMVoltage(a, 'D10', abs(m(1)));
-            writePWMVoltage(a, 'D11', 0);
+            writePWMVoltage(a, dForwardPin, abs(m(1)));
+            writePWMVoltage(a, dBackwardPin, 0);
         else
-            writePWMVoltage(a, 'D10', 0);
-            writePWMVoltage(a, 'D11', 0);
+            writePWMVoltage(a, dForwardPin, 0);
+            writePWMVoltage(a, dBackwardPin, 0);
         end
 
-        v = readVoltage(a,'A7');
+        v = readVoltage(a,aPositionPin);
         c(2) = v;
         pos(pos_cont) = c(2);
         pos_cont = pos_cont + 1;
         e(2) = r(2)-c(2);
         m(2) = (1/(2*T))*((2*T*Kp+Ki*(T^2)+4*Kd)*e(2) + (2*Ki*(T^2)-8*Kd)*e(1));
         if abs(m(2)) > 5
-            writePWMVoltage(a, 'D10', 5);
-            writePWMVoltage(a, 'D11', 0);
+            writePWMVoltage(a, dForwardPin, 5);
+            writePWMVoltage(a, dBackwardPin, 0);
         elseif abs(m(2)) > 5
-            writePWMVoltage(a, 'D10', abs(m(2)));
-            writePWMVoltage(a, 'D11', 0);
+            writePWMVoltage(a, dForwardPin, abs(m(2)));
+            writePWMVoltage(a, dBackwardPin, 0);
         else
-            writePWMVoltage(a, 'D10', 0);
-            writePWMVoltage(a, 'D11', 0);
+            writePWMVoltage(a, dForwardPin, 0);
+            writePWMVoltage(a, dBackwardPin, 0);
         end
 
         %Main loop, calculation of process output c(k), error e(k), and manipulation m(k)
         k = 3;
         while v < .90 || v > .95 %k=3:1:N
-            v = readVoltage(a,'A7');
+            v = readVoltage(a,aPositionPin);
             c(k) = v;
             pos(pos_cont) = c(k);
             pos_cont = pos_cont + 1;
@@ -78,32 +82,32 @@ while(1)
             m(k) = (1/(2*T))*((2*T*m(k-2)) + ((2*T*Kp+Ki*(T^2)+4*Kd)*e(k)) + ((2*Ki*(T^2)-8*Kd)*e(k-1)) + ((-2*T*Kp+Ki*(T^2)+4*Kd)*e(k-2)));
             if e(k) > 0
                 if abs(m(k)) > 5
-                    writePWMVoltage(a, 'D10', 5);
-                    writePWMVoltage(a, 'D11', 0);
+                    writePWMVoltage(a, dForwardPin, 5);
+                    writePWMVoltage(a, dBackwardPin, 0);
                 elseif abs(m(k)) > 2
-                    writePWMVoltage(a, 'D10', abs(m(k)));
-                    writePWMVoltage(a, 'D11', 0);
+                    writePWMVoltage(a, dForwardPin, abs(m(k)));
+                    writePWMVoltage(a, dBackwardPin, 0);
                 else
-                    writePWMVoltage(a, 'D10', 0);
-                    writePWMVoltage(a, 'D11', 0);
+                    writePWMVoltage(a, dForwardPin, 0);
+                    writePWMVoltage(a, dBackwardPin, 0);
                 end
             else
                 if abs(m(k)) > 5
-                    writePWMVoltage(a, 'D10', 0);
-                    writePWMVoltage(a, 'D11', 5);
+                    writePWMVoltage(a, dForwardPin, 0);
+                    writePWMVoltage(a, dBackwardPin, 5);
                 elseif abs(m(k)) > 2
-                    writePWMVoltage(a, 'D10', 0);
-                    writePWMVoltage(a, 'D11', abs(m(k)));
+                    writePWMVoltage(a, dForwardPin, 0);
+                    writePWMVoltage(a, dBackwardPin, abs(m(k)));
                 else
-                    writePWMVoltage(a, 'D10', 0);
-                    writePWMVoltage(a, 'D11', 0);
+                    writePWMVoltage(a, dForwardPin, 0);
+                    writePWMVoltage(a, dBackwardPin, 0);
                 end
             end
             k = k + 1;
         end
 
-        writeDigitalPin(a, 'D11', 0);
-        writeDigitalPin(a, 'D10', 0);
+        writeDigitalPin(a, dForwardPin, 0);
+        writeDigitalPin(a, dBackwardPin, 0);
 %         % Plots
 %         T=1*(1:k-1);
 %         subplot(3,1,1),plot(T,c,'r-');
@@ -134,44 +138,44 @@ while(1)
         c = [];
         e = [];
         m = [];
-        v = readVoltage(a,'A7');
+        v = readVoltage(a,aPositionPin);
         c(1) = v;
         pos(pos_cont) = c(1);
         pos_cont = pos_cont + 1;
         e(1) = r(1)-c(1);
         m(1) = (1/(2*T))*((2*T*Kp+Ki*(T^2)+4*Kd)*e(1));
         if abs(m(1)) > 5
-            writePWMVoltage(a, 'D10', 5);
-            writePWMVoltage(a, 'D11', 0);
+            writePWMVoltage(a, dForwardPin, 5);
+            writePWMVoltage(a, dBackwardPin, 0);
         elseif abs(m(1)) > 2
-            writePWMVoltage(a, 'D10', abs(m(1)));
-            writePWMVoltage(a, 'D11', 0);
+            writePWMVoltage(a, dForwardPin, abs(m(1)));
+            writePWMVoltage(a, dBackwardPin, 0);
         else
-            writePWMVoltage(a, 'D10', 0);
-            writePWMVoltage(a, 'D11', 0);
+            writePWMVoltage(a, dForwardPin, 0);
+            writePWMVoltage(a, dBackwardPin, 0);
         end
 
-        v = readVoltage(a,'A7');
+        v = readVoltage(a,aPositionPin);
         c(2) = v;
         pos(pos_cont) = c(2);
         pos_cont = pos_cont + 1;
         e(2) = r(2)-c(2);
         m(2) = (1/(2*T))*((2*T*Kp+Ki*(T^2)+4*Kd)*e(2) + (2*Ki*(T^2)-8*Kd)*e(1));
         if abs(m(2)) > 5
-            writePWMVoltage(a, 'D10', 5);
+            writePWMVoltage(a, dForwardPin, 5);
             writePWMVoltage(a, 'D11', 0);
         elseif abs(m(2)) > 5
-            writePWMVoltage(a, 'D10', abs(m(2)));
+            writePWMVoltage(a, dForwardPin, abs(m(2)));
             writePWMVoltage(a, 'D11', 0);
         else
-            writePWMVoltage(a, 'D10', 0);
+            writePWMVoltage(a, dForwardPin, 0);
             writePWMVoltage(a, 'D11', 0);
         end
 
         %Main loop, calculation of process output c(k), error e(k), and manipulation m(k)
         k = 3;
         while v < .42 || v > .49 
-            v = readVoltage(a,'A7');
+            v = readVoltage(a,aPositionPin);
             c(k) = v;
             pos(pos_cont) = c(k);
             pos_cont = pos_cont + 1;
@@ -179,32 +183,32 @@ while(1)
             m(k) = (1/(2*T))*((2*T*m(k-2)) + ((2*T*Kp+Ki*(T^2)+4*Kd)*e(k)) + ((2*Ki*(T^2)-8*Kd)*e(k-1)) + ((-2*T*Kp+Ki*(T^2)+4*Kd)*e(k-2)));
             if e(k) > 0
                 if abs(m(k)) > 5
-                    writePWMVoltage(a, 'D10', 5);
-                    writePWMVoltage(a, 'D11', 0);
+                    writePWMVoltage(a, dForwardPin, 5);
+                    writePWMVoltage(a, dBackwardPin, 0);
                 elseif abs(m(k)) > 2
-                    writePWMVoltage(a, 'D10', abs(m(k)));
-                    writePWMVoltage(a, 'D11', 0);
+                    writePWMVoltage(a, dForwardPin, abs(m(k)));
+                    writePWMVoltage(a, dBackwardPin, 0);
                 else
-                    writePWMVoltage(a, 'D10', 0);
-                    writePWMVoltage(a, 'D11', 0);
+                    writePWMVoltage(a, dForwardPin, 0);
+                    writePWMVoltage(a, dBackwardPin, 0);
                 end
             else
                 if abs(m(k)) > 5
-                    writePWMVoltage(a, 'D10', 0);
-                    writePWMVoltage(a, 'D11', 5);
+                    writePWMVoltage(a, dForwardPin, 0);
+                    writePWMVoltage(a, dBackwardPin, 5);
                 elseif abs(m(k)) > 2
-                    writePWMVoltage(a, 'D10', 0);
-                    writePWMVoltage(a, 'D11', abs(m(k)));
+                    writePWMVoltage(a, dForwardPin, 0);
+                    writePWMVoltage(a, dBackwardPin, abs(m(k)));
                 else
-                    writePWMVoltage(a, 'D10', 0);
-                    writePWMVoltage(a, 'D11', 0);
+                    writePWMVoltage(a, dForwardPin, 0);
+                    writePWMVoltage(a, dBackwardPin, 0);
                 end
             end
             k = k + 1;
         end
 
-        writeDigitalPin(a, 'D11', 0);
-        writeDigitalPin(a, 'D10', 0);
+        writeDigitalPin(a, dForwardPin, 0);
+        writeDigitalPin(a, dBackwardPin, 0);
 
 %         % Plots
 %         figure
@@ -237,44 +241,44 @@ while(1)
         c = [];
         e = [];
         m = [];
-        v = readVoltage(a,'A7');
+        v = readVoltage(a,aPositionPin);
         c(1) = v;
         pos(pos_cont) = c(1);
         pos_cont = pos_cont + 1;
         e(1) = r(1)-c(1);
         m(1) = (1/(2*T))*((2*T*Kp+Ki*(T^2)+4*Kd)*e(1));
         if abs(m(1)) > 5
-            writePWMVoltage(a, 'D10', 5);
-            writePWMVoltage(a, 'D11', 0);
+            writePWMVoltage(a, dForwardPin, 5);
+            writePWMVoltage(a, dBackwardPin, 0);
         elseif abs(m(1)) > 2
-            writePWMVoltage(a, 'D10', abs(m(1)));
-            writePWMVoltage(a, 'D11', 0);
+            writePWMVoltage(a, dForwardPin, abs(m(1)));
+            writePWMVoltage(a, dBackwardPin, 0);
         else
-            writePWMVoltage(a, 'D10', 0);
-            writePWMVoltage(a, 'D11', 0);
+            writePWMVoltage(a, dForwardPin, 0);
+            writePWMVoltage(a, dBackwardPin, 0);
         end
 
-        v = readVoltage(a,'A7');
+        v = readVoltage(a,aPositionPin);
         c(2) = v;
         pos(pos_cont) = c(2);
         pos_cont = pos_cont + 1;
         e(2) = r(2)-c(2);
         m(2) = (1/(2*T))*((2*T*Kp+Ki*(T^2)+4*Kd)*e(2) + (2*Ki*(T^2)-8*Kd)*e(1));
         if abs(m(2)) > 5
-            writePWMVoltage(a, 'D10', 5);
-            writePWMVoltage(a, 'D11', 0);
+            writePWMVoltage(a, dForwardPin, 5);
+            writePWMVoltage(a, dBackwardPin, 0);
         elseif abs(m(2)) > 5
-            writePWMVoltage(a, 'D10', abs(m(2)));
-            writePWMVoltage(a, 'D11', 0);
+            writePWMVoltage(a, dForwardPin, abs(m(2)));
+            writePWMVoltage(a, dBackwardPin, 0);
         else
-            writePWMVoltage(a, 'D10', 0);
-            writePWMVoltage(a, 'D11', 0);
+            writePWMVoltage(a, dForwardPin, 0);
+            writePWMVoltage(a, dBackwardPin, 0);
         end
 
         %Main loop, calculation of process output c(k), error e(k), and manipulation m(k)
         k = 3;
         while v < .90 || v > .95 
-            v = readVoltage(a,'A7');
+            v = readVoltage(a,aPositionPin);
             c(k) = v;
             pos(pos_cont) = c(k);
             pos_cont = pos_cont + 1;
@@ -282,32 +286,32 @@ while(1)
             m(k) = (1/(2*T))*((2*T*m(k-2)) + ((2*T*Kp+Ki*(T^2)+4*Kd)*e(k)) + ((2*Ki*(T^2)-8*Kd)*e(k-1)) + ((-2*T*Kp+Ki*(T^2)+4*Kd)*e(k-2)));
             if e(k) > 0
                 if abs(m(k)) > 5
-                    writePWMVoltage(a, 'D10', 5);
-                    writePWMVoltage(a, 'D11', 0);
+                    writePWMVoltage(a, dForwardPin, 5);
+                    writePWMVoltage(a, dBackwardPin, 0);
                 elseif abs(m(k)) > 2
-                    writePWMVoltage(a, 'D10', abs(m(k)));
-                    writePWMVoltage(a, 'D11', 0);
+                    writePWMVoltage(a, dForwardPin, abs(m(k)));
+                    writePWMVoltage(a, dBackwardPin, 0);
                 else
-                    writePWMVoltage(a, 'D10', 0);
-                    writePWMVoltage(a, 'D11', 0);
+                    writePWMVoltage(a, dForwardPin, 0);
+                    writePWMVoltage(a, dBackwardPin, 0);
                 end
             else
                 if abs(m(k)) > 5
-                    writePWMVoltage(a, 'D10', 0);
-                    writePWMVoltage(a, 'D11', 5);
+                    writePWMVoltage(a, dForwardPin, 0);
+                    writePWMVoltage(a, dBackwardPin, 5);
                 elseif abs(m(k)) > 2
-                    writePWMVoltage(a, 'D10', 0);
-                    writePWMVoltage(a, 'D11', abs(m(k)));
+                    writePWMVoltage(a, dForwardPin, 0);
+                    writePWMVoltage(a, dBackwardPin, abs(m(k)));
                 else
-                    writePWMVoltage(a, 'D10', 0);
-                    writePWMVoltage(a, 'D11', 0);
+                    writePWMVoltage(a, dForwardPin, 0);
+                    writePWMVoltage(a, dBackwardPin, 0);
                 end
             end
             k = k + 1;
         end
 
-        writeDigitalPin(a, 'D11', 0);
-        writeDigitalPin(a, 'D10', 0);
+        writeDigitalPin(a, dForwardPin, 0);
+        writeDigitalPin(a, dBackwardPin, 0);
 
 %         % Plots
 %         figure
@@ -340,44 +344,44 @@ while(1)
         c = [];
         e = [];
         m = [];
-        v = readVoltage(a,'A7');
+        v = readVoltage(a,aPositionPin);
         c(1) = v;
         pos(pos_cont) = c(1);
         pos_cont = pos_cont + 1;
         e(1) = r(1)-c(1);
         m(1) = (1/(2*T))*((2*T*Kp+Ki*(T^2)+4*Kd)*e(1));
         if abs(m(1)) > 5
-            writePWMVoltage(a, 'D10', 5);
-            writePWMVoltage(a, 'D11', 0);
+            writePWMVoltage(a, dForwardPin, 5);
+            writePWMVoltage(a, dBackwardPin, 0);
         elseif abs(m(1)) > 2
-            writePWMVoltage(a, 'D10', abs(m(1)));
-            writePWMVoltage(a, 'D11', 0);
+            writePWMVoltage(a, dForwardPin, abs(m(1)));
+            writePWMVoltage(a, dBackwardPin, 0);
         else
-            writePWMVoltage(a, 'D10', 0);
-            writePWMVoltage(a, 'D11', 0);
+            writePWMVoltage(a, dForwardPin, 0);
+            writePWMVoltage(a, dBackwardPin, 0);
         end
 
-        v = readVoltage(a,'A7');
+        v = readVoltage(a,aPositionPin);
         c(2) = v;
         pos(pos_cont) = c(2);
         pos_cont = pos_cont + 1;
         e(2) = r(2)-c(2);
         m(2) = (1/(2*T))*((2*T*Kp+Ki*(T^2)+4*Kd)*e(2) + (2*Ki*(T^2)-8*Kd)*e(1));
         if abs(m(2)) > 5
-            writePWMVoltage(a, 'D10', 5);
-            writePWMVoltage(a, 'D11', 0);
+            writePWMVoltage(a, dForwardPin, 5);
+            writePWMVoltage(a, dBackwardPin, 0);
         elseif abs(m(2)) > 5
-            writePWMVoltage(a, 'D10', abs(m(2)));
-            writePWMVoltage(a, 'D11', 0);
+            writePWMVoltage(a, dForwardPin, abs(m(2)));
+            writePWMVoltage(a, dBackwardPin, 0);
         else
-            writePWMVoltage(a, 'D10', 0);
-            writePWMVoltage(a, 'D11', 0);
+            writePWMVoltage(a, dForwardPin, 0);
+            writePWMVoltage(a, dBackwardPin, 0);
         end
 
         %Main loop, calculation of process output c(k), error e(k), and manipulation m(k)
         k = 3;
         while v < 1.35 || v > 1.45 
-            v = readVoltage(a,'A7');
+            v = readVoltage(a,aPositionPin);
             c(k) = v;
             pos(pos_cont) = c(k);
             pos_cont = pos_cont + 1;
@@ -385,32 +389,32 @@ while(1)
             m(k) = (1/(2*T))*((2*T*m(k-2)) + ((2*T*Kp+Ki*(T^2)+4*Kd)*e(k)) + ((2*Ki*(T^2)-8*Kd)*e(k-1)) + ((-2*T*Kp+Ki*(T^2)+4*Kd)*e(k-2)));
             if e(k) > 0
                 if abs(m(k)) > 5
-                    writePWMVoltage(a, 'D10', 5);
-                    writePWMVoltage(a, 'D11', 0);
+                    writePWMVoltage(a, dForwardPin, 5);
+                    writePWMVoltage(a, dBackwardPin, 0);
                 elseif abs(m(k)) > 2
-                    writePWMVoltage(a, 'D10', abs(m(k)));
-                    writePWMVoltage(a, 'D11', 0);
+                    writePWMVoltage(a, dForwardPin, abs(m(k)));
+                    writePWMVoltage(a, dBackwardPin, 0);
                 else
-                    writePWMVoltage(a, 'D10', 0);
-                    writePWMVoltage(a, 'D11', 0);
+                    writePWMVoltage(a, dForwardPin, 0);
+                    writePWMVoltage(a, dBackwardPin, 0);
                 end
             else
                 if abs(m(k)) > 5
-                    writePWMVoltage(a, 'D10', 0);
-                    writePWMVoltage(a, 'D11', 5);
+                    writePWMVoltage(a, dForwardPin, 0);
+                    writePWMVoltage(a, dBackwardPin, 5);
                 elseif abs(m(k)) > 2
-                    writePWMVoltage(a, 'D10', 0);
-                    writePWMVoltage(a, 'D11', abs(m(k)));
+                    writePWMVoltage(a, dForwardPin, 0);
+                    writePWMVoltage(a, dBackwardPin, abs(m(k)));
                 else
-                    writePWMVoltage(a, 'D10', 0);
-                    writePWMVoltage(a, 'D11', 0);
+                    writePWMVoltage(a, dForwardPin, 0);
+                    writePWMVoltage(a, dBackwardPin, 0);
                 end
             end
             k = k + 1;
         end
 
-        writeDigitalPin(a, 'D11', 0);
-        writeDigitalPin(a, 'D10', 0);
+        writeDigitalPin(a, dForwardPin, 0);
+        writeDigitalPin(a, dBackwardPin, 0);
 
 %         % Plots
 %         figure
